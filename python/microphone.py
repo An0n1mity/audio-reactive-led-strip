6db01f8764b15a6976e5ac7ac06877e239f0640d
An0n1mity <monthouel@et.esiea.fr>
2023-10-13 20:33:19 +0200

    Switching between mic and loopback

--- python/microphone.py
+++ python/microphone.py
@@ -3,36 +3,49 @@ import numpy as np
 import pyaudiowpatch as pyaudio
 import config
 
+# constant to define if loopback device is used or microphone
+USE_LOOPBACK = False
 
 def start_stream(callback):
     p = pyaudio.PyAudio()
     # Get default WASAPI info
-    wasapi_info = p.get_host_api_info_by_type(pyaudio.paWASAPI)
-    # Get default WASAPI speakers
-    default_speakers = p.get_device_info_by_index(wasapi_info["defaultOutputDevice"])
-    if not default_speakers["isLoopbackDevice"]:
-        for loopback in p.get_loopback_device_info_generator():
-            if default_speakers["name"] in loopback["name"]:
-                default_speakers = loopback
-                break
-    
-    frames_per_buffer = int(config.MIC_RATE / config.FPS)
-    stream = p.open(format=pyaudio.paInt16,
-                    channels=default_speakers['maxInputChannels'],
+    if USE_LOOPBACK:
+        wasapi_info = p.get_host_api_info_by_type(pyaudio.paWASAPI)
+        # Get default WASAPI speakers
+        default_speakers = p.get_device_info_by_index(wasapi_info["defaultOutputDevice"])
+        if not default_speakers["isLoopbackDevice"]:
+            for loopback in p.get_loopback_device_info_generator():
+                if default_speakers["name"] in loopback["name"]:
+                    default_speakers = loopback
+                    break
+        
+        frames_per_buffer = int(config.MIC_RATE / config.FPS)
+        stream = p.open(format=pyaudio.paInt16,
+                        channels=default_speakers['maxInputChannels'],
+                        rate=config.MIC_RATE,
+                        input=True,
+                        input_device_index=default_speakers['index'],
+                        frames_per_buffer=frames_per_buffer
+                        )
+    else:
+        frames_per_buffer = int(config.MIC_RATE / config.FPS)
+        stream = p.open(format=pyaudio.paInt16,
+                    channels=1,
                     rate=config.MIC_RATE,
                     input=True,
-                    input_device_index=default_speakers['index'],
-                    frames_per_buffer=frames_per_buffer
-                    )
+                    frames_per_buffer=frames_per_buffer)
+
     overflows = 0
     prev_ovf_time = time.time()
 
     while True:
         try:
-            #y = np.fromstring(stream.read(frames_per_buffer, exception_on_overflow=False), dtype=np.int16)
-            #y = y.astype(np.float32)
-            data = stream.read(frames_per_buffer, exception_on_overflow=False)
-            y = np.frombuffer(data, dtype=np.int16)[::default_speakers['maxInputChannels']].astype(np.float32) 
+            if USE_LOOPBACK:
+                data = stream.read(frames_per_buffer, exception_on_overflow=False)
+                y = np.frombuffer(data, dtype=np.int16)[::default_speakers['maxInputChannels']].astype(np.float32) 
+            else:
+                y = np.fromstring(stream.read(frames_per_buffer, exception_on_overflow=False), dtype=np.int16)
+                y = y.astype(np.float32)
             callback(y)
         except IOError:
             overflows += 1
